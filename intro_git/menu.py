b927395626c7c16b2545bd4898ad89ddcf6cf0cb
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2025-02-26 20:35:56 -0300

    feat: implementa no menu a função de subtração

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -1,8 +1,12 @@
 import calculadora
 
 def menu():
-    escolha = int(input("Selecione uma das opções:\n1. Somar\n"))
+    escolha = int(input("Selecione uma das opções:\n1. Somar\n2. Subtrair\n"))
     if escolha == 1:
         a = int(input("Insira o primeiro número para a soma: "))
         b = int(input("Insira o segundo número para a soma: "))
         print(calculadora.somar(a, b))
+    if escolha == 2:
+        a = int(input("Insira o primeiro número para a subtração: "))
+        b = int(input("Insira o segundo número para a subtração: "))
+        print(calculadora.subtrair(a, b))
