a30dc28574da9fd0ec1ad383d0d6bde3b7776be3
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2025-02-26 22:34:19 -0300

    feat: implementa a função de divisão

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -1,7 +1,7 @@
 import calculadora
 
 def menu():
-    escolha = int(input("Selecione uma das opções:\n1. Somar\n2. Subtrair\n3. Multiplicar\n"))
+    escolha = int(input("Selecione uma das opções:\n1. Somar\n2. Subtrair\n3. Multiplicar\n4. Dividir\n"))
     if escolha == 1:
         a = int(input("Insira o primeiro número para a soma: "))
         b = int(input("Insira o segundo número para a soma: "))
