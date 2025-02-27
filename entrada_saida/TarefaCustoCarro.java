965ac178e3bd3cc512dcac2f47878e20fb5f11fa
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2025-02-27 18:23:14 -0300

    feat: calcula o valor de um carro novo
    - recebe e mostra os dados por meio da linha de comando

--- entrada_saida/TarefaCustoCarro.java
+++ entrada_saida/TarefaCustoCarro.java
@@ -1,14 +1,16 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class TarefaCustoCarro{
     public static void main(String[] args){
         //declaração de variáveis
+        Scanner leitor = new Scanner(System.in);
         double a, resultado;
         //entrada de dados
-        a = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de fábrica do carro:"));
+        System.out.println("Digite o valor de fábrica do carro:");
+        a = leitor.nextDouble();
         //processamento
         resultado = a + (a * .28) + (a * .45);
         //saída
-        JOptionPane.showMessageDialog(null, resultado);
+        System.out.printf("Valor = %f", resultado);
     }
 }
