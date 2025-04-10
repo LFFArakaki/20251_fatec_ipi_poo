bc379c38bfa450fb0ebb8199cc07ad42ad4105f3
Luis e Mariana <luisfellipe.arakaki@gmail.com>
2025-04-10 17:07:31 -0300

    feat: implementa escolha aleatoria no teste

--- colecoes/TesteVetorDinamicoV1.java
+++ colecoes/TesteVetorDinamicoV1.java
@@ -4,18 +4,19 @@ import java.util.Random;
 
 public class TesteVetorDinamicoV1 {
     public static void main(String[] args) throws Exception {
-        var v = new VetorDinamico();
+        var vetor = new VetorDinamico();
         var rand = new Random();
 
         while(true)
         {
-            var escolha = rand.nextDouble(0);
+            var escolha = rand.nextDouble();
             var numero = rand.nextInt(1,7);
-            v.adicionar(numero);
-            System.out.println(v);
+            var posicao = vetor.getQuantidade() == 0 ? 0 : rand.nextInt(vetor.getQuantidade());
+            if(escolha <= 0.5) vetor.adicionar(numero);
+            else vetor.remover(posicao);
+            System.out.println(vetor);
             System.out.println("***********************");
             Thread.sleep(5000);
         }
-
     }
 }
