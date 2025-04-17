1ae369c0d6e5a54fd99f67bf74b7942963aca776
Luis e Mariana <luisfellipe.arakaki@gmail.com>
2025-04-17 17:36:36 -0300

    feat(ap-008): encerra a apostila
    - fazer prova na semana que vem

--- colecoes/MyTrakz.java
+++ colecoes/MyTrakz.java
@@ -1,11 +1,21 @@
 package colecoes;
 
+import java.util.Collections;
 import java.util.ArrayList;
-
+import java.util.Comparator;
 import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.JOptionPane;
 
-class Track{
+class OrganizaPorAvaliacao implements Comparator<Track> {
+    public int compare(Track track1, Track track2){
+        
+        if(track1.getAvaliacao() > track2.getAvaliacao()) return -1;
+        if(track1.getAvaliacao() < track2.getAvaliacao()) return 1;
+        return track1.compareTo(track2);
+    }
+}
+
+class Track implements Comparable<Track> {
     private String titulo;
     private int avaliacao;
     public Track(String titulo, int avaliacao){
@@ -14,6 +24,29 @@ class Track{
     }
     public Track(String titulo){
         this.titulo = titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public int getAvaliacao(){
+        return this.avaliacao;
+    }
+    public void setAvaliacao(int avaliacao){
+        this.avaliacao = avaliacao;
+        return;
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        var outraTrack = (Track)obj;
+        return this.titulo.equals(outraTrack.titulo);
+    }
+    public int compareTo(Track outraTrack){
+        return this.titulo.compareTo(outraTrack.titulo);
     }
     public String toString(){
         return String.format("Titulo: %s, Avaliacao: %d",titulo, avaliacao);
@@ -31,23 +64,81 @@ public class MyTrakz {
             switch(op)
             {
                 case 1:
+                {
                     var titulo = JOptionPane.showInputDialog("Titulo?");
                     minhasTracks.add(new Track(titulo));
                     JOptionPane.showMessageDialog(null, "Track adicionada");
                     break;
+                }
                 case 2:
+                {
+                    var titulo = JOptionPane.showInputDialog("Titulo?");
+                    var desejada = new Track(titulo);
+                    if(!minhasTracks.contains(desejada)) JOptionPane.showMessageDialog(null, "Musica nao encontrada");
+                    else
+                    {
+                        minhasTracks.remove(desejada);
+                        JOptionPane.showMessageDialog(null, "Musica removida!");
+                    }
                     break;
+                }
                 case 3:
+                {
+                    var titulo = JOptionPane.showInputDialog("Titulo?");
+                    var desejada = new Track(titulo);
+                    var posicao = minhasTracks.indexOf(desejada);
+                    if(posicao == -1) JOptionPane.showMessageDialog(null, "Musica nao encontrada");
+                    else
+                    {
+                        var novoTitulo = JOptionPane.showInputDialog("Novo titulo?");
+                        minhasTracks.get(posicao).setTitulo(novoTitulo);
+                        JOptionPane.showMessageDialog(null, "Musica atuaizada!");
+                    }
                     break;
+                }
                 case 4:
-
+                {
+                    if(minhasTracks.isEmpty()) System.out.println("Lista vazia!");
+                    else
+                    {
+                        Collections.sort(minhasTracks);
+                        JOptionPane.showMessageDialog(null, minhasTracks);                    
+                    }
                     break;
+                }
                 case 5:
+                {
+                    if(minhasTracks.isEmpty()) System.out.println("Lista vazia!");
+                    else
+                    {
+                        Collections.sort(minhasTracks, new OrganizaPorAvaliacao());
+                        JOptionPane.showMessageDialog(null, minhasTracks);                    
+                    }
                     break;
+                }
+                case 6:
+                {
+                    var titulo = JOptionPane.showInputDialog("Titulo?");
+                    var desejada = new Track(titulo);
+                    var posicao = minhasTracks.indexOf(desejada);
+                    if(posicao == -1) JOptionPane.showMessageDialog(null, "Musica nao encontrada");
+                    else
+                    {
+                        var avaliacao = Integer.parseInt(JOptionPane.showInputDialog("Nota?"));
+                        minhasTracks.get(posicao).setAvaliacao(avaliacao);
+                        JOptionPane.showMessageDialog(null, "Musica avaliada!");
+                    }
+                }
                 case 0:
+                {
+                    JOptionPane.showMessageDialog(null, "Xau!");
                     break;
+                }
                 default:
+                {
+                    JOptionPane.showMessageDialog(null, "Opcao invalida!");
                     break;
+                }
             }
         } while(op !=0);
     }
